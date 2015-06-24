71ed4083dbe6d822ae784bf8058ae3d6af4dccca
c0d3rm0nk3y <code4chaosmobile@gmail.com>
2015-06-24 07:30:53 -0500

    added some links to official api and npm modules to the archive.org.js node module

--- util/archive.org.js
+++ util/archive.org.js
@@ -2,4 +2,24 @@
  * this will utilize internet archive.org api
  * 
  * this should open up access to the video and audio clips
+ * 
+ * https://www.npmjs.com/package/archive.org
+ * npm install archive.org --save
+ * 
+ * Example
+   Archive = require('archive.org');
+   archive = new Archive();
+    
+   archive.search({q: 'Grateful Dead'}, function(err, res) {
+     console.log(res);
+   });
+   
+   here is something from their own site on their api..
+   
+   https://archive.org/advancedsearch.php?q=bunny+AND+licenseurl:[http://creativecommons.org/a+TO+http://creativecommons.org/z]
+                                          &fl[]=identifier,title,mediatype,collection&rows=15&output=json
+                                          &callback=IAE.search_hits
+ 
+ * https://archive.org/advancedsearch.php#raw
  **/
+ 
